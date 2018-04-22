f3ebee2807c76f83cd69b4ab7d321f0737d514e4
hman1123 <38593130+hman1123@users.noreply.github.com>
2018-04-21 23:17:52 -0400

    Added Keyboard

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 	public static void main(String [] args) {
-		System.out.println("Hello");
-		System.out.println("Bye");
+		Scanner keyboard = new Scanner(System.in);
+		int x = keyboard.nextInt();
+		System.out.println(LazyBoy(x));
+		
+	}
+	public static String LazyBoy(int y) {
+		String orange="";
+		if(y == 1) {
+			orange="Hello";
+		}
+		else if (y == 2) {
+			orange = "mustang";
+		}
+		else if (y == 3) {
+			orange = "ice cream";
+		}
+		else {
+			orange = "fortnite";
+		}
+		return orange;
 	}
 }
